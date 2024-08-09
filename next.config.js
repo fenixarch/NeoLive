5927df88aa26a0d4477190022ce06604347864d4
FenixOffc <fenixoffc@mail.ru>
2024-08-09 20:35:54 +0300

    update-1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
-  },
-};
-
-module.exports = nextConfig;
+    experimental: {
+      appDir: true,
+      swcPlugins: [["next-superjson-plugin", {}]]
+    },
+    images: {
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        dusk: "#242526",
-        lightgray: "#3a3b3c",
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: "class",
-    }),
-  ],
-  darkMode: "class",
-};
+    extend: {}
+   },
+  
+  plugins: [],
+}
